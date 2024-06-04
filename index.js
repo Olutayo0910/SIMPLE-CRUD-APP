f0b9f126547f6364eba9b41e4a8634fec5624ea3
Olutayo0910 <olutayoogunlade0910@gmail.com>
2024-06-04 16:09:10 +0100

    setting up database

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/OlutayoDB'
+
 const app = express()
 
-app.listen(3000, () => {
-    console.log('running........')
-});
+mongoose.connect(url)
+const con = mongoose.connection
+
+con.on('open', function(){
+    console.log('Testing connection')
+})
